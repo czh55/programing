a51445e61400b0ac9afe3d131aca2092e9b78497
czh <czh55@qq.com>
2018-05-29 15:53:41 +0800

    删除redisson实现的分布式锁，使用原生的就行

--- src/main/java/com/programing/common/Const.java
+++ src/main/java/com/programing/common/Const.java
@@ -21,8 +21,8 @@ public class Const {
     }
 
     public interface Cart{
-        int CHECKED = 1;//即购物车选中状态
-        int UN_CHECKED = 0;//购物车中未选中状态
+        int CHECKED = 1;//即收藏夹选中状态
+        int UN_CHECKED = 0;//收藏夹中未选中状态
 
         String LIMIT_NUM_FAIL = "LIMIT_NUM_FAIL";
         String LIMIT_NUM_SUCCESS = "LIMIT_NUM_SUCCESS";
